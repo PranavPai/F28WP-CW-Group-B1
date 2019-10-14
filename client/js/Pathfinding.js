64b25e3e77fe61972612654962a29a7613e9ca0a
cm66 <cm66@hw.ac.uk>
2019-10-14 10:48:19 +0400

    Updated Pathfind.js script a bit

--- client/js/Pathfinding.js
+++ client/js/Pathfinding.js
@@ -9,5 +9,7 @@
 * - The tiles we need to look at
 * when a tile is checked it we look at and calculate:
 * - the distance from the starting postion, to prevent the player from looking to far away from the target.
-* - 
+* - distance valuse to the target. Will be needed so we know what to check first next.
+* - the movment penalty for moving onto that tile. EG it is faster to walk over stone then mud.
+* after taking all this into account we can deside on what tile to move to next.
 */
