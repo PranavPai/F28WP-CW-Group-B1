06be8f7f8faf610afa852da3834a659c8961ee4b
Benjamin Jacob Reji <bjr5@hw.ac.uk>
2019-11-13 14:20:58 +0400

    mongodb with player collections added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const PORTNO = '2000';
 
 var express = require('express');
+var database = require('./server/js/database')
+
 var app = express();
 var serv = require('http').Server(app);
 
@@ -12,3 +14,27 @@ app.use(express.static(__dirname + '/client'));
 serv.listen(PORTNO);
 
 console.log(`Server Started on ${PORTNO}`);
+
+// --------------------------------------------------------------
+var Player = require('./server/js/models/player')
+
+var defaultplayer = new Player({
+    firstname: "Default",
+    lastname: "Player",
+    email: "defaultplayer@test.com",
+    username: "defaultplayer",
+    password: "default",
+    playerposition: {
+      x: 50,
+      y: 50
+    },
+    isOnline: true
+  });
+  
+  defaultplayer.save()
+    .then(doc => {
+      console.log(doc)
+    })
+    .catch(err => {
+      console.error(err.code)
+    })
