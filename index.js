488289b3ec0c39c01bb9ee4e219f7fa1788b3ba9
Benjamin Jacob Reji <bjr5@hw.ac.uk>
2019-11-13 18:16:11 +0400

    adding socket.io

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
 
-var database = require('./server/js/database');
-var Player = require('./server/js/models/player');
-
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
@@ -17,4 +14,13 @@ serv.listen(PORTNO);
 console.log(`Server Started on ${PORTNO}`);
 
 
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket) {
+    console.log('socket connection');
+});
+
+
 // --------------------------------------------------------------
+
+var database = require('./server/js/database');
+var Player = require('./server/js/models/player');
