33f025edc36d7a1b770f34f26ca4ec300908009f
Hitesh <hm95@hw.ac.uk>
2019-11-23 20:00:55 +0400

    added working login_menu

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const PORTNO = '2000';
 
 var express = require('express');
@@ -11,4 +12,15 @@ app.use(express.static(__dirname + '/client'));
 
 serv.listen(PORTNO);
 
-console.log(`Server Started on ${PORTNO}`);
+
+//console.log(`Server Started on ${PORTNO}`);
+=======
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+>>>>>>> Stashed changes
+ 
