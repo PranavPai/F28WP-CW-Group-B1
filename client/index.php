c3be1ea23f123320a6a0262c324e59085c34c05e
Waleed Hesham Rashed <whr1@hw.ac.uk>
2019-11-24 19:11:49 +0400

    chatsystem login 2

--- client/index.php
+++ client/index.php
@@ -4,10 +4,10 @@
 <title> Chat System </title>
 <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
- 
+
 <div id="wrapper">
     <div id="menu">
-        <p class="welcome"> Welcome, <b></b></p>
+    <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
         <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
         <div style="clear:both"></div>
     </div>
@@ -20,6 +20,84 @@
     </form>
 </div>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+<script type="text/javascript">
+
+$(document).ready(function(){
+	//If user wants to end the session
+	$("#exit").click(function(){
+		var exit = confirm("Are you sure you want to end the session?");
+		if(exit==true){window.location = 'index.php?logout=true';}		
+	});
+});
+</script>
+<?phpif(isset($_GET['logout'])){ 
+     
+     //Simple exit message
+     $fp = fopen("log.html", 'a');
+     fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
+     fclose($fp);
+      
+     session_destroy();
+     header("Location: index.php"); //Redirect the user
+ }
+ ?>
+
+<?
+// Since we are using a cookie-based session to store the name, we must call session_start() before anything is outputted to the browser
+session_start();
+// a simple login form which asks the user for his/her name 
+function loginForm(){
+    echo'
+    <div id="loginform">
+    <form action="index.php" method="post">
+        <p>Please enter your name to continue:</p>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" />
+        <input type="submit" name="enter" id="enter" value="Enter" />
+    </form>
+    </div>
+    ';
+}
+// an if and else statement to verify that the person entered a name
+// If the person entered a name, we set that name as $_SESSION['name'] 
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+?>
+
+<?php // showing the login form in case a user has not logged in
+if(!isset($_SESSION['name'])){
+    loginForm();
+}
+else{
+?>
+<div id="wrapper">
+    <div id="menu">
+        <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+        <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+        <div style="clear:both"></div>
+    </div>    
+    <div id="chatbox"></div>
+     
+    <form name="message" action="">
+        <input name="usermsg" type="text" id="usermsg" size="63" />
+        <input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
+    </form>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+<script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+});
+</script>
+<?php
+}
+?>
 <script type="text/javascript">
 // jQuery Document
 $(document).ready(function(){
